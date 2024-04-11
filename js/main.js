bdfd8877a848a47df94281fb03c7044e686ff818
joe sunshine <joesun02@gmail.com>
2024-04-11 11:21:47 -0400

    main.js should work as intended

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-let data, barchart, parallelCoordinates;
+let data, barchart, linechart;
 
-d3.csv('Agrofood_co2_emissions.csv').then(_data => {
+d3.csv('data/Agrofood_co2_emission.csv').then(_data => {
     data = _data;
-    const keys = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g'];
 
-    
+    data = data.filter(dataPoint =>{
+        return Object.values(dataPoint).every(value => value !== 'NA');
+    });
+     
+
+    console.log("reading csv")
     barchart = new BarChart({ parentElement: ".bar-chart" }, data);
     linechart = new LineChart({ parentElement: ".line-chart" }, data);
 
