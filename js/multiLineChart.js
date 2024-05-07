c24a443774dd383bdf546352ba552c9e7649b0bb
ramsai0408 <ramsainimmagadda1999@gmail.com>
2024-05-07 13:02:11 -0400

    added some interactive changes and legends to line chart

--- js/multiLineChart.js
+++ js/multiLineChart.js
@@ -17,14 +17,17 @@ class MultiLineChart {
         vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
         vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
+        // Create an SVG container
         vis.svg = d3.select(vis.config.parentElement)
             .append('svg')
             .attr('width', vis.config.containerWidth)
             .attr('height', vis.config.containerHeight);
 
+        // Add the main chart group
         vis.chartG = vis.svg.append('g')
             .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
 
+        // Set up scales
         vis.xScale = d3.scaleTime()
             .range([0, vis.width]);
 
@@ -33,9 +36,11 @@ class MultiLineChart {
 
         vis.colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
+        // Create axes
         vis.xAxis = d3.axisBottom(vis.xScale);
         vis.yAxis = d3.axisLeft(vis.yScale);
 
+        // Append axis groups
         vis.xAxisG = vis.chartG.append('g')
             .attr('class', 'axis x-axis')
             .attr('transform', `translate(0, ${vis.height})`);
@@ -43,59 +48,109 @@ class MultiLineChart {
         vis.yAxisG = vis.chartG.append('g')
             .attr('class', 'axis y-axis');
 
+        // Group for lines and tooltips
         vis.marksG = vis.chartG.append('g');
+
+        // Tooltip setup
+        vis.tooltip = d3.select('body').append('div')
+            .attr('class', 'tooltip')
+            .style('opacity', 0);
+
+        // Add a legend group
+        vis.legendG = vis.svg.append('g')
+            .attr('class', 'legend')
+            .attr('transform', `translate(${vis.width - 150}, 30)`);
+
+        // Initially, all lines are shown
+        vis.activeLines = {};
     }
 
-  
     updateVis(parameters) {
         let vis = this;
-    
+
         // Update scales
         vis.xScale.domain(d3.extent(vis.data, d => d.Year));
         vis.yScale.domain([0, d3.max(vis.data, d => d3.max(parameters, parameter => d[parameter]))]);
         vis.colorScale.domain(parameters);
-    
+
         // Update axes
         vis.xAxisG.transition().call(vis.xAxis);
         vis.yAxisG.transition().call(vis.yAxis);
-    
-        // Bind data
+
+        // Initialize activeLines to show all parameters by default
+        parameters.forEach(p => {
+            if (vis.activeLines[p] === undefined) vis.activeLines[p] = true;
+        });
+
+        // Create line generator function
+        const lineGenerator = (parameter) => d3.line()
+            .x(data => vis.xScale(data.Year))
+            .y(data => vis.yScale(data[parameter]));
+
+        // Bind data to lines
         const lines = vis.marksG.selectAll('.line')
-            .data(parameters);
-    
-        // Enter & update
-        const lineGenerator = lines.enter().append('path')
+            .data(parameters.filter(p => vis.activeLines[p]));
+
+        // Enter & update paths
+        lines.enter().append('path')
             .attr('class', 'line')
             .merge(lines)
-            .transition()
             .attr('fill', 'none')
             .attr('stroke', d => vis.colorScale(d))
             .attr('stroke-width', 1.5)
-            .attr('d', d => {
-                const line = d3.line()
-                    .x(data => vis.xScale(data.Year))
-                    .y(data => vis.yScale(data[d]));
-                return line(vis.data);
+            .attr('d', d => lineGenerator(d)(vis.data))
+            .on('mouseover', function (event, d) {
+                d3.select(this).attr('stroke-width', 3); // Highlight the line
+                vis.tooltip.transition().duration(200).style('opacity', 0.9);
+                vis.tooltip.html(`<strong>${d}</strong>`)
+                    .style('left', (event.pageX + 5) + 'px')
+                    .style('top', (event.pageY - 28) + 'px');
             })
-            lineGenerator
-    .on('mouseover', (event, d) => {
-        d3.select(event.currentTarget)
-          .transition()
-          .duration(150)
-          .attr('stroke-width', 3)
-          .attr('stroke', d3.rgb(vis.colorScale(d)).brighter(0.5));
-    })
-    .on('mouseout', (event, d) => {
-        d3.select(event.currentTarget)
-          .transition()
-          .duration(150)
-          .attr('stroke-width', 1.5)
-          .attr('stroke', d => vis.colorScale(d));
-    });
-    
-        // Exit
+            .on('mouseout', function () {
+                d3.select(this).attr('stroke-width', 1.5); // Revert to normal width
+                vis.tooltip.transition().duration(500).style('opacity', 0);
+            })
+            .on('click', function () {
+                d3.selectAll('.line').attr('stroke-opacity', 0.2); // Dim all lines
+                d3.select(this).attr('stroke-opacity', 1).attr('stroke-width', 3); // Highlight clicked line
+            });
+
+        // Remove old lines
         lines.exit().remove();
-    
+
+        // Add a legend with clickable items
+        const legendItems = vis.legendG.selectAll('.legend-item')
+            .data(parameters);
+
+        // Enter & update legend items
+        const legendEnter = legendItems.enter().append('g')
+            .attr('class', 'legend-item')
+            .attr('transform', (d, i) => `translate(0, ${i * 20})`)
+            .on('click', function (event, d) {
+                vis.activeLines[d] = !vis.activeLines[d]; // Toggle visibility
+                vis.updateVis(parameters); // Re-render
+            });
+
+        legendEnter.append('rect')
+            .attr('width', 18)
+            .attr('height', 18)
+            .attr('fill', d => vis.colorScale(d))
+            .attr('opacity', d => (vis.activeLines[d] ? 1 : 0.2));
+
+        legendEnter.append('text')
+            .attr('x', 24)
+            .attr('y', 9)
+            .attr('dy', '0.35em')
+            .text(d => d);
+
+        // Update existing legend items
+        legendItems.select('rect')
+            .attr('opacity', d => (vis.activeLines[d] ? 1 : 0.2));
+
+        legendItems.select('text')
+            .text(d => d);
+
+        legendItems.exit().remove();
     }
 
     filterDataByCountry(country) {
